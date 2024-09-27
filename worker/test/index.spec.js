0a8cc547726a002813e69a588b482b6e00285b91
Kesara Rathnayake <kesara@fq.nz>
2024-09-27 17:47:36 +1200

    test: Add basic tests  (#7)
    * style: Fix code style issues
    
    * chore: Update test dependencies
    
    * test: Add basic tests
    
    * style: Convert tabs to spaces

--- worker/test/index.spec.js
+++ worker/test/index.spec.js
@@ -1,20 +1,28 @@
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
-import { describe, it, expect } from 'vitest';
-import worker from '../src';
+import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test'
+import { describe, it, expect } from 'vitest'
+import worker from '../src'
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new Request('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+describe('Datatracker redirect test', () => {
+  it('should redirect to DT with a 302 status', async () => {
+    const request = new Request('http://example.org')
+    const ctx = createExecutionContext()
+    const response = await worker.fetch(request, env, ctx)
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch(request, env, ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-});
+    await waitOnExecutionContext(ctx)
+
+    expect(response.status).toBe(302)
+    expect(response.headers.get('Location')).toBe('https://datatracker.ietf.org/meeting/past')
+  })
+})
+
+describe('404 test', () => {
+  it('should return 404 for /foobar', async () => {
+    const request = new Request('http://example.org/foobar')
+    const ctx = createExecutionContext()
+    const response = await worker.fetch(request, env, ctx)
+
+    await waitOnExecutionContext(ctx)
+
+    expect(response.status).toBe(404)
+  })
+})
